486201ff74909beffb457a6d5cc291538f6bd1ab
wkl007 <499657357@qq.com>
2020-05-18 11:24:03 +0000

    Deploying to gh-pages from master @ 0a5c15f630cfc13845e04bfab3b693364d5a2cb8 🚀

---
+++ precache-manifest.3f79d9d7f620f5050c71b4e8fe8a745b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9ca80217f881869fd7",
+    "url": "/ant-design-vue-admin/css/app.362bc83b.css"
+  },
+  {
+    "revision": "b93a7fd57371a0090507",
+    "url": "/ant-design-vue-admin/css/chunk-vendors.5fcb7a2d.css"
+  },
+  {
+    "revision": "55d856c7b9ffc5a88829",
+    "url": "/ant-design-vue-admin/css/login.7018e453.css"
+  },
+  {
+    "revision": "5825f033c6ff12cd1ed1f3c99dff5e4b",
+    "url": "/ant-design-vue-admin/img/background.5825f033.svg"
+  },
+  {
+    "revision": "c924155285cdba2f57cbd751674c1bb0",
+    "url": "/ant-design-vue-admin/img/logo.c9241552.png"
+  },
+  {
+    "revision": "f31032253edf454e1280c009ce0277a5",
+    "url": "/ant-design-vue-admin/img/logo.f3103225.svg"
+  },
+  {
+    "revision": "98b017e6211f41441f447046f9d951df",
+    "url": "/ant-design-vue-admin/index.html"
+  },
+  {
+    "revision": "6e9ca80217f881869fd7",
+    "url": "/ant-design-vue-admin/js/app.8aeb7845.js"
+  },
+  {
+    "revision": "b93a7fd57371a0090507",
+    "url": "/ant-design-vue-admin/js/chunk-vendors.e20baeec.js"
+  },
+  {
+    "revision": "762c4daf964d6b68e8cadbcf18b4b59e",
+    "url": "/ant-design-vue-admin/js/chunk-vendors.e20baeec.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd2e8adb386917d18cd",
+    "url": "/ant-design-vue-admin/js/company.6f849b00.js"
+  },
+  {
+    "revision": "de5b8b47e5510d58ef89",
+    "url": "/ant-design-vue-admin/js/exception403.c03aa83c.js"
+  },
+  {
+    "revision": "fcadc20168e501e83283",
+    "url": "/ant-design-vue-admin/js/exception500.e7cadce4.js"
+  },
+  {
+    "revision": "555a24e634d47ca89bf6",
+    "url": "/ant-design-vue-admin/js/fail.03ebeac5.js"
+  },
+  {
+    "revision": "55d856c7b9ffc5a88829",
+    "url": "/ant-design-vue-admin/js/login.337ab860.js"
+  },
+  {
+    "revision": "f926ad1fc909adab60d35ea68e06fbad",
+    "url": "/ant-design-vue-admin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ant-design-vue-admin/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ant-design-vue-admin/precache-manifest.f051bbd5ab992bbde7c968ccf26063ee.js"
+  "/ant-design-vue-admin/precache-manifest.3f79d9d7f620f5050c71b4e8fe8a745b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ant-design-vue-admin"});
